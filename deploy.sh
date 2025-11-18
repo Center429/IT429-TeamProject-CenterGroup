55169b92d21f650d770bba048689a47024bbe22f
rabugha <rabugha@gmu.edu>
2025-11-17 22:11:49 -0500

    Update the deploy.sh for my branch

--- deploy.sh
+++ deploy.sh
@@ -420,7 +420,7 @@ EOF
         BUILD_IMAGE="aws/codebuild/amazonlinux-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_SMALL"
         PRIVILEGED_MODE="false"
-        SOURCE_VERSION="pdf2html-subtree"  # Use pdf2html-subtree since buildspec only exists there
+        SOURCE_VERSION="raad-vuln-004-fix1"  # Use pdf2html-subtree since buildspec only exists there
         BUILDSPEC_FILE="buildspec-unified.yml"
     else
         BUILD_IMAGE="aws/codebuild/amazonlinux2-x86_64-standard:5.0"
